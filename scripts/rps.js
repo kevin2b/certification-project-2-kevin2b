27ad923b9eaa5e2dab6b8ff05cb60e60ecc362a8
kevin2b <25062561+kevin2b@users.noreply.github.com>
2025-06-08 22:13:05 -0400

    Add JS for game flow

--- scripts/rps.js
+++ scripts/rps.js
@@ -6,8 +6,8 @@
   #CPUGameScore = 0;
   #playerRoundScore = 0;
   #CPURoundScore = 0;
-  #message = "";
-  #metaMessage = `Game: ${this.#game}. Round: ${this.#round}`;
+  #message = "Choose wisely";
+  #metaMessage = `Game:${this.#game} Round:${this.#round}`;
   
   /**
    * Play a rock, paper, scissor round. Game message for current round is stored in #message. If current round is same as TOTAL_ROUNDS,
@@ -17,12 +17,12 @@
    * @param {Number} CPUChoiceNum Integer 0, 1, or 2 representing Rock, Paper or Scissor respectively. Rock is used if invalid input.
    */
   playRound(playerChoiceNum, CPUChoiceNum){
-    const DICTIONARY = {0: "Rock", 1: "Paper", 2: "Scissor"};
-    const playerChoice = DICTIONARY?.[playerChoiceNum] || "Rock";
-    const CPUChoice = DICTIONARY?.[CPUChoiceNum] || "Rock";
+    
+    const playerChoice = this.convertChoice(playerChoiceNum);
+    const CPUChoice = this.convertChoice(CPUChoiceNum);
 
     if (this.#round > RPS.TOTAL_ROUNDS){
-      this.#newGame();
+      this.newGame();
     }
 
     if (playerChoice === CPUChoice){
@@ -40,6 +40,7 @@
       this.#setMessage(`${playerChoice} loses to ${CPUChoice}.`);
     }  
     this.#appendMessage(` ${this.#playerRoundScore}:${this.#CPURoundScore}`);
+    this.#metaMessage = `Game:${this.#game} Round:${this.#round}`;
 
     if (this.#round === RPS.TOTAL_ROUNDS){
       this.#updateGameResult();
@@ -54,16 +55,63 @@
     this.#CPUGameScore = 0;
     this.#playerRoundScore = 0;
     this.#CPURoundScore = 0;
-    this.#message = "";
+    this.#message = "Choose wisely";
     this.#metaMessage = `Game: ${this.#game}. Round: ${this.#round}`;
   }
 
-  #newGame(){
-    this.#game++;
-    this.#round = 1;
-    this.#playerRoundScore = 0;
-    this.#CPURoundScore = 0;
-    this.#message = "";
+  /**
+   * Generate randomly 0, 1, or 2
+   * @returns Random integer from 0 to 2 inclusive
+   */
+  generateCPUChoice(){
+    return Math.floor(Math.random() * 3);
+  }
+
+  /**
+   * Convert integer 0, 1, or 2 to Rock, Paper or Scissor respectively. Rock is used if invalid input.
+   * @param {Number} choiceNum Integer 0, 1, or 2 representing Rock, Paper or Scissor respectively
+   * @returns String representation of choice
+   */
+  convertChoice(choiceNum){
+    const DICTIONARY = {0: "Rock", 1: "Paper", 2: "Scissor"};
+    return DICTIONARY?.[choiceNum] || "Rock";;
+  }
+
+  getMessages(){
+    return(
+      {
+        message: this.#message,
+        metaMessage: this.#metaMessage
+      }
+    );
+  }
+
+  getGameScores(){
+    return(
+      {
+        playerGameScore: this.#playerGameScore,
+        CPUGameScore: this.#CPUGameScore
+      }
+    )
+  }
+
+  /**
+   * Return true if current game is over.
+   * @returns true if current game is over
+   */
+  isGameOver(){
+    return this.#round > RPS.TOTAL_ROUNDS;
+  }
+
+
+  newGame(){
+    const saveGame = this.#game + 1;
+    const saveCPUGameScore = this.#CPUGameScore;
+    const saveplayerGameScore = this.#playerGameScore;
+    this.reset();
+    this.#game = saveGame;
+    this.#CPUGameScore = saveCPUGameScore;
+    this.#playerGameScore = saveplayerGameScore;
     this.#metaMessage = `Game: ${this.#game}. Round: ${this.#round}`;
   }
 
@@ -71,28 +119,20 @@
    * Update game result based on overall round score. +1 for win, +0.5 for draw.
    */
   #updateGameResult() {
-    this.#metaMessage = `Game: ${this.#game}.`;
+    this.#metaMessage = `Game:${this.#game}`;
     if (this.#playerRoundScore > this.#CPURoundScore) {
       this.#playerGameScore++;
-      this.#metaMessage += " You Win!"
+      this.#metaMessage += ` You Win! ${this.#playerRoundScore}:${this.#CPURoundScore}`;
     }
     else if (this.#CPURoundScore > this.#playerRoundScore) {
       this.#CPUGameScore++;
-      this.#metaMessage += " You Lose!"
+      this.#metaMessage += ` You Lose! ${this.#playerRoundScore}:${this.#CPURoundScore}`;
     }
     else {
       this.#playerGameScore += 0.5;
       this.#CPUGameScore += 0.5;
-      this.#metaMessage += " Game Draw!"
+      this.#metaMessage += ` Game Draw! ${this.#playerRoundScore}:${this.#CPURoundScore}`
     }
-  }
-
-  /**
-   * 
-   * @returns Random integer from 0 to 2 inclusive
-   */
-  generateCPUChoice(){
-    return Math.floor(Math.random() * 3);
   }
 
   #setMessage(message){
--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,175 @@
 import {RPS} from "./rps.js";
 
 const rps = new RPS();
+const playButton = document.querySelector("#play");
+const metaInfo = document.querySelector("#meta-info");
+const roundInfo = document.querySelector("#round-info");
+const HIDDEN_CLASS = "js-hidden";
+const playerOptions = document.querySelector("#player-options");
+const CPUOption = document.querySelectorAll(".js-enemy-option");
+const playerOption = document.querySelectorAll(".js-player-option");
+const gameOverButtons = document.querySelector("#game-over");
+const playAgainButton = document.querySelector("#play-again");
+const resetButton = document.querySelector("#reset");
+
+
+window.addEventListener("pageshow", () =>{
+  togglePlayerButtons(false);
+});
+
+//Activate the game on play
+playButton.addEventListener("click", ()=>{
+  metaInfo.classList.remove(HIDDEN_CLASS);
+  roundInfo.classList.remove(HIDDEN_CLASS);
+  playButton.classList.add(HIDDEN_CLASS);
+  synchronizeGameState();
+  togglePlayerButtons(true);
+});
+
+//Handle player selection of rock, paper, scissor
+playerOptions.addEventListener("click", (e) => {
+  const playerChoice = e.target.closest("button");
+  var playerChoiceNum = 0;
+
+  //If what user clicked is not a button or disabled, do nothing
+  if (playerChoice === null || playerChoice.disabled){
+    return;
+  }
+
+  if (playerChoice.id === "player-rock"){
+    playerChoiceNum = 0;
+  }
+  if (playerChoice.id === "player-paper"){
+    playerChoiceNum = 1;
+  }
+  if (playerChoice.id === "player-scissor"){
+    playerChoiceNum = 2;
+  }
+
+  const CPUChoiceNum = rps.generateCPUChoice();
+  rps.playRound(playerChoiceNum, CPUChoiceNum);
+  synchronizeGameState();
+  outlineOptionChoices(playerChoiceNum, CPUChoiceNum);
+  showSelectedChoices(playerChoiceNum, CPUChoiceNum);
+
+  if (rps.isGameOver()){
+    displayGameOverState();
+  }
+});
+
+playAgainButton.addEventListener("click", () =>{
+  metaInfo.classList.remove(HIDDEN_CLASS);
+  roundInfo.classList.remove(HIDDEN_CLASS);
+  gameOverButtons.classList.add(HIDDEN_CLASS);
+  resetOptionChoices();
+  resetSelectedChoices();
+  rps.newGame();
+  synchronizeGameState();
+  togglePlayerButtons(true);
+});
+
+resetButton.addEventListener("click", () => {
+  metaInfo.classList.add(HIDDEN_CLASS);
+  roundInfo.classList.add(HIDDEN_CLASS);
+  gameOverButtons.classList.add(HIDDEN_CLASS);
+  playButton.classList.remove(HIDDEN_CLASS);
+  resetOptionChoices();
+  resetSelectedChoices();
+  rps.reset();
+  synchronizeGameState();
+  togglePlayerButtons(false);
+});
+
+
+/**
+ * Make the DOM state reflect current game state
+ */
+function synchronizeGameState() {
+  const CPUGameScore = document.querySelector("#enemy-score");
+  const playerGameScore = document.querySelector("#player-score");
+  const messages = rps.getMessages();
+  const gameScores = rps.getGameScores();
+  CPUGameScore.textContent = gameScores.CPUGameScore;
+  playerGameScore.textContent = gameScores.playerGameScore;
+  metaInfo.textContent = messages.metaMessage;
+  roundInfo.textContent = messages.message;
+  return;
+}
+
+/**
+ * Enable player choices in DOM if enable is true, disable if false
+ * @param {boolean} enable enable player choice buttons if true, disable if false
+ */
+function togglePlayerButtons(enable){
+  const playerButtons = document.querySelectorAll(".player__option");
+  playerButtons.forEach((playerButton) => {
+    playerButton.disabled = !enable;
+  })
+}
+
+function outlineOptionChoices(playerChoiceNum, CPUChoiceNum){
+  resetOptionChoices();
+  CPUOption.forEach((selected, index) => {
+    if (index === CPUChoiceNum){
+      selected.classList.add("js-enemy-outline");
+    }
+  });
+  playerOption.forEach((selected, index) => {
+    if (index === playerChoiceNum){
+      selected.classList.add("js-player-outline");
+    }
+  });
+}
+
+function resetOptionChoices(){
+  CPUOption.forEach((selected) => {
+    selected.classList.remove("js-enemy-outline");
+  });
+  playerOption.forEach((selected) => {
+    selected.classList.remove("js-player-outline");
+  });
+}
+
+function showSelectedChoices(playerChoiceNum, CPUChoiceNum){
+  const enemySelected = document.querySelectorAll(".js-enemy-selected");
+  const playerSelected = document.querySelectorAll(".js-player-selected");
+  enemySelected.forEach((option, index) => {
+    if (index === CPUChoiceNum) {
+      option.classList.remove(HIDDEN_CLASS);
+      void option.offsetWidth;
+      option.classList.add("js-show");
+    } else {
+      option.classList.remove("js-show");
+      option.classList.add(HIDDEN_CLASS);
+    }
+  });
+  playerSelected.forEach((option, index) => {
+    if (index === playerChoiceNum) {
+      option.classList.remove(HIDDEN_CLASS);
+      void option.offsetWidth;
+      option.classList.add("js-show");
+    } else {
+      option.classList.remove("js-show");
+      option.classList.add(HIDDEN_CLASS);
+    }
+  });
+}
+
+function resetSelectedChoices(){
+  const enemySelected = document.querySelectorAll(".js-enemy-selected");
+  const playerSelected = document.querySelectorAll(".js-player-selected");
+  enemySelected.forEach((option, index) => {
+    option.classList.remove("js-show");
+    option.classList.add(HIDDEN_CLASS);
+  });
+  playerSelected.forEach((option, index) => {
+    option.classList.remove("js-show");
+    option.classList.add(HIDDEN_CLASS);
+  });
+}
+
+function displayGameOverState(){
+  roundInfo.classList.add(HIDDEN_CLASS);
+  gameOverButtons.classList.remove(HIDDEN_CLASS);
+  togglePlayerButtons(false);
+}
